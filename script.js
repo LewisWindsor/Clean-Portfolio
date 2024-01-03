469a1e967030a4db7e672d05854092b635e69c23
LewisWindsor <100640865+LewisWindsor@users.noreply.github.com>
2024-01-02 19:13:06 -0500

    Updated Hamburger, added tutorial code to cross examine for error

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+/*NOT MY CODE BELOW*/
+
+/*function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}*/
